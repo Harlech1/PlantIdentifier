40e69f82efcae6dd8e870718ae8b972892f81fff
Harlech1 <turkerkizilcik@gmail.com>
2024-12-23 19:04:44 +0300

    initial commit

---
+++ PlantIdentifier/PlantIdentifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantIdentifierApp.swift
+//  PlantIdentifier
+//
+//  Created by Türker Kızılcık on 23.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct PlantIdentifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
